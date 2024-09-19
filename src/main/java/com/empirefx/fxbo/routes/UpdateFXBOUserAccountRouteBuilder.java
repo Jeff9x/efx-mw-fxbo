cd0d705e92049771df2eeb6a7823f656595142d3
Emmanuel Muchiri <emmanuel.muchiri@empirefx.com>
2024-09-19 13:19:03 +0300

    Update User working.Awaiting refactoring

---
+++ src/main/java/com/empirefx/fxbo/routes/UpdateFXBOUserAccountRouteBuilder.java
@@ -0,0 +1,54 @@
+package com.empirefx.fxbo.routes;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.ValidationException;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@Component
+public class UpdateFXBOUserAccountRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+// Global exception handler for validation errors
+        onException(ValidationException.class)
+                .log("Validation failed: ${exception.message}")
+                .handled(true)
+                .setBody(simple("Validation failed: ${exception.message}"));
+
+        rest()
+                .post("/update-user")
+                .description("Adapter REST Service")
+                .consumes(APPLICATION_JSON_VALUE)
+                .produces("application/json")
+                .to("direct:updateUserAccount");
+
+        from("direct:updateUserAccount").routeId("com.empirefx.request.dispatchRequest4")
+                .noStreamCaching().noMessageHistory().noTracing()
+                .setHeader("Content-Type", constant("application/json"))
+                .setHeader("Accept", constant("application/json"))
+                .process("headersSetterProcessor")
+                .convertBodyTo(String.class)
+                .marshal().json()
+//                .process("userRequestValidationProcessor")
+                .doTry()
+                .log(LoggingLevel.INFO, "\n Calling FXBO Endpoint :: Update User Account Request :: {{atomic1.uriUpdateUser}}")
+                .enrich().simple("{{atomic1.uriUpdateUser}}").id("callServiceBack4")
+                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                .to("direct:fetchUpdateUserAccountsResponse");
+
+        from("direct:fetchUpdateUserAccountsResponse")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
+                .log("Processed response with content type: ${header.Content-Type}")
+                .removeHeaders("*")
+                .removeHeader("Authorization")
+                .doTry()
+                .setBody(simple("${body}"))
+                .convertBodyTo(String.class)
+                .unmarshal().json()
+                .end();
+    }
+}
