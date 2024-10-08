5bd2532e69a915ec024c245522301cb4f180c50c
Emmanuel Muchiri <emmanuel.muchiri@empirefx.com>
2024-10-08 15:40:08 +0300

    fixed bug

---
+++ src/main/java/com/empirefx/fxbo/processors/CreateTradingAccountHeadersSetterProcessor.java
@@ -0,0 +1,40 @@
+package com.empirefx.fxbo.processors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+import static com.empirefx.fxbo.commonlib.constants.ConstantsCommons.CALLER_IP;
+import static com.empirefx.fxbo.commonlib.constants.ConstantsCommons.EMPTY;
+
+@Component
+public class CreateTradingAccountHeadersSetterProcessor implements Processor {
+    @Value("${adaptive.authorization}")
+    private String authorization;
+    @Value("${adaptive.token}")
+    private String token;
+    @Value("${adaptive.camelHttpMethod}")
+    private String camelHttpMethod;
+    @Value("${adaptive.method}")
+    private String getCamelHttpMethod;
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        // Get Caller IP
+        HttpServletRequest request = exchange.getIn().getBody(HttpServletRequest.class);
+
+        String ipRemote = Objects.isNull(request) ? EMPTY : request.getRemoteAddr();
+
+        // Set the Bearer token in the Authorization header
+        exchange.getIn().setHeader(CALLER_IP, ipRemote);
+        exchange.getIn().setHeader("Authorization", token);
+        System.out.println("Authorization Header Set:");
+
+        System.out.println("Incoming Caller IP :"+ipRemote);
+    }
+
+
+}
